8718b79828358520b820fb32274125401a2ba485
Aytekin Topçu <aytekint68@gmail.com>
2026-06-23 17:45:11 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -390,7 +390,7 @@ async def mesaj_isleyici(update: Update, context: ContextTypes.DEFAULT_TYPE):
         yanit = (
             f"📂 <b>DOSYA BİLGİLERİ</b>\n\n<b>No:</b> {row['Arama_Sutunu']}\n━━━━━━━━━━━━━━━━━━\n"
             f"📅 <b>Başvuru Tarihi:</b> {row.get('Başvuru Tarihi', '')}\n⏳ <b>Sonraki Aşama (Termen):</b> {termen}\n"
-            f"📝 <b>Kurum Notu:</b> {kurum_notu}\n\n📂 <b>Kaynak:</b> {kaynak_dosya_metni}\n━━━━━━━━━━━━━━━━━━\n"
+            f"📝 <b>Kurum Notu:</b> {kurum_notu}\n📂 <b>Kaynak:</b> {kaynak_dosya_metni}\n━━━━━━━━━━━━━━━━━━\n"
             f"⚖️ <b>KARAR (ORDIN) DURUMU</b>\n\n"
         )
 
